596084609fcc92afe4b805ec5223f21db2409d30
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-12-01 13:05:14 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
+
+// Remove A, AN, THE
+const strip = band => band.replace(/^(the |an |a )/i, '');
+
+const sorted = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+// Render
+document.querySelector('#band').innerHTML =
+  sorted.map(b => `<li>${b}</li>`).join('');
